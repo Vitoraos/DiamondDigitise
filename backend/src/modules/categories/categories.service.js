b5fb5e940599c20e55fff287a0ed2fbf6ba49644
Vitora OS <emmykarl07@gmail.com>
2026-05-06 17:14:24 +0100

    feat: add backend/src/modules/categories/categories.service.js

---
+++ backend/src/modules/categories/categories.service.js
@@ -0,0 +1,12 @@
+
+const supabase = require('../../config/supabase');
+
+exports.getAll = async () => {
+  const { data, error } = await supabase
+    .from('categories')
+    .select('*')
+    .order('name', { ascending: true });
+  
+  if (error) throw new Error('DATABASE_ERROR');
+  return data;
+};
