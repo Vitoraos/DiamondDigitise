bbc36449520cfeb5de2788cbcf271d30f8dcbb59
Vitora OS <emmykarl07@gmail.com>
2026-05-06 02:07:22 +0100

    feat: add frontendd/app/(auth)/loading.tsx

---
+++ frontendd/app/(auth)/loading.tsx
@@ -0,0 +1,8 @@
+
+export default function AuthLoading() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <p>Loading authentication...</p>
+    </div>
+  );
+}
