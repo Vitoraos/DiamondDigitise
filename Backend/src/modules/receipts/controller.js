8885190d57a6214dfd8a04f1dbf18ee28166fb12
Vitora OS <emmykarl07@gmail.com>
2026-05-25 09:09:18 +0100

    Add async list method to receipts controller

--- Backend/src/modules/receipts/controller.js
+++ Backend/src/modules/receipts/controller.js
@@ -1,9 +1,11 @@
-// src/modules/receipts/receipts.controller.js
 'use strict';
-
 const receiptsService = require('./receipts.service');
 
 const controller = {
+  async list(req, res) {
+    const receipts = await receiptsService.listReceipts();
+    res.json({ data: receipts });
+  },
   async getByBooking(req, res) {
     const receipt = await receiptsService.getReceiptByBookingId(req.params.bookingId);
     res.json({ data: receipt });
