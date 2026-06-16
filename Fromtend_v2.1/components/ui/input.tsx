353f1c97627bdcacf18ccba2c297dc2fa94932e4
Vitora OS <emmykarl07@gmail.com>
2026-06-16 04:34:09 +0100

    Add Input component with forwardRef support

---
+++ Fromtend_v2.1/components/ui/input.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex w-full bg-surface border border-white/[0.12] px-4 py-3.5 text-sm text-white placeholder:text-dim focus:border-gold focus:outline-none transition-colors disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
